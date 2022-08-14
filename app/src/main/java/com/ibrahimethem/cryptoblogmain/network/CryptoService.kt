5631f8743c5720bb8c4f9d05c4fbe9fbdf5dfc81
ibrahim <ibrahim-ethem-50@hotmail.com>
2022-08-14 14:17:06 +0300

    latest

--- app/src/main/java/com/ibrahimethem/cryptoblogmain/network/CryptoService.kt
+++ app/src/main/java/com/ibrahimethem/cryptoblogmain/network/CryptoService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface CryptoService {
-    @GET("v1/cryptocurrency/listings/")
+    @GET("v1/cryptocurrency/listings/latest")
     suspend fun getAllCryptoList(
         @Header("X-CMC_PRO_API_KEY") apiKey : String,
         @Query("limit") limit : String
