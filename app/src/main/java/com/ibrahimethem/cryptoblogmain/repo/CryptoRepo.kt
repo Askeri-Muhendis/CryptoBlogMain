b0b8aa7f7400d1db8f03a8b71f03eba0ab33d6ea
ibrahim <ibrahim-ethem-50@hotmail.com>
2022-08-14 14:20:21 +0300

    repo düzeltme

--- app/src/main/java/com/ibrahimethem/cryptoblogmain/repo/CryptoRepo.kt
+++ app/src/main/java/com/ibrahimethem/cryptoblogmain/repo/CryptoRepo.kt
@@ -2,10 +2,12 @@ package com.ibrahimethem.cryptoblogmain.repo
 
 import com.ibrahimethem.cryptoblogmain.model.CryptoList
 import com.ibrahimethem.cryptoblogmain.network.CryptoService
+import okio.IOException
 import retrofit2.Response
 
 class CryptoRepo(private val cryptoService : CryptoService) {
     suspend fun getAllCrypto(apiKey : String,limit : String) : Response<CryptoList> {
+        //throw IOException()
         return cryptoService.getAllCryptoList(apiKey,limit)
     }
 }
