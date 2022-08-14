230ddcebaf085ab3a62ea97af291d7b037721b18
İbrahim Ethem Şen <68695185+Askeri-Muhendis@users.noreply.github.com>
2022-08-14 14:16:14 +0300

    Delete Resource.kt

--- app/src/main/java/com/ibrahimethem/cryptoblogmain/util/Resource.kt
+++
@@ -1,7 +0,0 @@
-package com.ibrahimethem.cryptoblogmain.util
-
-sealed class Resource<T>{
-    class Success<T>(val data : T) : Resource<T>()
-    class Error<T>(val errorCode : Int,val errorMessage : String) : Resource<T>()
-    class Exception<T>(val exception : Throwable) : Resource<T>()
-}
